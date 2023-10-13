3cebf104e8538a8f52b4f498ac111a5d9011df38
Andrey <cliff_06@bk.ru>
2023-10-13 19:30:25 +0500

    создали файл task_41

---
+++ homework/task_41/Program.cs
@@ -0,0 +1,96 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры числа через запятую. Посчитайте, сколько чисел больше 0 ввёл пользователь. 0, 7, 8, -2, -2 -> 2 -1, -7, 567, 89, 223-> 3
+// Console.Write("Введите числа через запятую: ");
+
+int[] numbers = StringToNum(Console.ReadLine());
+PrintArray(numbers);
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
+
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int[count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input[i] != ',')
+        {
+            if (i != input.Length - 1)
+            {
+                temp += input[i].ToString();
+                i++;
+            }
+            else
+            {
+                temp += input[i].ToString();
+                break;
+            }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+
+
+
+// Console.Write(«Введите количество элементов массива: «);
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[] Array = new int[m];
+
+// void mas(int m)
+// {
+// for (int i = 0; i < m; i++)
+// {
+// Console.WriteLine($»Введите {i+1} элемент массива «);
+// Array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+
+// }
+
+// int kol(int[] Array)
+// {
+// int i=0;
+// int sum = 0;
+// while (i < Array.Length)
+// {
+// if(Array[i]>0)
+// sum = sum + 1;
+// i = i + 1;
+// }
+// return sum;
+// }
+
+// mas(m);
+// Console.Write($»\n Чисел больше нуля: {kol(Array)}»);
